1e9053f72c370f6df9c0afc785c66a5fd096523a
Albert Simanjuntak <albertsimanjuntak@Alberts-MacBook-Air.local>
2025-07-04 23:35:33 +0700

    add stage

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         String name = System.getenv("NAME");
-        System.out.println("Hello world1!" + (name != null ? ", " + name : "") + "!");
+        System.out.println("Hello worldwdw1!" + (name != null ? ", " + name : "") + "!");
     }
 }
